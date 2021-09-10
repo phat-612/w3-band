b4d0bb338047568e90c95111890a4ca0ebe0ca97
Your Name <you@example.com>
2021-09-10 10:59:39 +0700

    add

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,8 @@
 // Bắt đầu: show menu header
 // đóng/mở menu
-var header = document.getElementById('header')
-var mobileMenu = document.getElementById('mobile-menu')
-var checkOC = header.clientHeight
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var checkOC = header.clientHeight;
 mobileMenu.onclick = function(){
     var menuClosed = header.clientHeight == checkOC;
     if (menuClosed) {
@@ -12,11 +12,16 @@ mobileMenu.onclick = function(){
     }
 }
 // đóng menu khi click nav
-var menuItems = document.querySelectorAll('#nav li a[href*="#"]')
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
 for (var i = 0; i < menuItems.length; i++) {
     var menuItem = menuItems[i];
-    menuItem.onclick = function(){
-        header.style = 'null';
+    menuItem.onclick = function(event){
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+        }else{
+            header.style = 'null';
+        }
     }
 } 
 
@@ -26,23 +31,23 @@ for (var i = 0; i < menuItems.length; i++) {
 
 
 // Bắt đầu: mở cửa sổ buy ticket
-const buyBtns = document.querySelectorAll('.js-buy-ticket')
-const modal = document.querySelector('.js-modal')
-const modalContainer = document.querySelector('.js-modal-container')
-const modalClose = document.querySelector('.js-modal-close')
+const buyBtns = document.querySelectorAll('.js-buy-ticket');
+const modal = document.querySelector('.js-modal');
+const modalContainer = document.querySelector('.js-modal-container');
+const modalClose = document.querySelector('.js-modal-close');
 function showBuyTickets(){
-    modal.classList.add('open')
+    modal.classList.add('open');
 }
 function hideBuyTickets(){
-    modal.classList.remove('open')
+    modal.classList.remove('open');
 }
 
 for (const buyBtn of buyBtns){
-    buyBtn.addEventListener('click', showBuyTickets)
+    buyBtn.addEventListener('click', showBuyTickets);
 }
-modalClose.addEventListener('click', hideBuyTickets)
-modal.addEventListener('click', hideBuyTickets)
+modalClose.addEventListener('click', hideBuyTickets);
+modal.addEventListener('click', hideBuyTickets);
 modalContainer.addEventListener('click', function(event){
-    event.stopPropagation()
+    event.stopPropagation();
 })
 // Kết thúc: mở cửa sổ buy ticket
